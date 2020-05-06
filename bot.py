2541dfdb2cec692d45ee023ee69233704162d712
reechee <richardokonicha@gmail.com>
2020-05-06 01:45:02 +0100

    mvp

---
+++ bot.py
@@ -0,0 +1,56 @@
+import telethon
+import os
+from flask import Flask, request
+import telebot
+from telebot import types
+from parser import pasig
+server = Flask(__name__)
+TOKEN = os.getenv("TOKEN")
+URL = os.getenv("URL")
+
+
+bot = telebot.TeleBot(TOKEN, threaded=True)
+
+@bot.message_handler(commands=["begin"])
+def begin(message):
+    chat_id = message.chat.id
+    text = "this is the start"
+    bot.send_message(
+        chat_id,
+        text=text
+    )
+
+@bot.channel_post_handler(commands=None, regexp=None, func=lambda message: message.content_type == "text",content_types=None)
+def begin_chan(message):
+    chat_id = message.chat.id
+    rawsignal = message.text
+    signal = pasig(rawsignal)
+    raw_id = -1001313782946
+    if signal:
+        bot.send_message(
+            chat_id,
+            text=signal,
+            parse_mode="html"
+        )
+    else:
+        pass
+
+@server.route("/"+TOKEN, methods=['POST'])
+def getMessage():
+    request_object = request.stream.read().decode("utf-8")
+    update_to_json = [telebot.types.Update.de_json(request_object)]
+    bot.process_new_updates(update_to_json)
+    return "got Message bro"
+
+@server.route("/hook")
+def webhook():
+    url=URL
+    bot.remove_webhook()
+    bot.set_webhook(url + TOKEN)
+    return f"Webhook set to {url}"
+# bot.get_chat_members_count
+
+# bot.remove_webhook()
+# bot.polling()
+if __name__ == "__main__":
+    server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
---
+++ parser.py
@@ -0,0 +1,66 @@
+
+import re
+
+def bparse(line):
+    parser = re.search("^(BUY)\s([A-Z]*)\s[\(@at\s]*([0-9]*[.,][0-9]*)[\).]", line)
+    if parser != None:
+        p = parser.groups()
+        output = f"""
+#{p[1]}
+{p[0]}📈{p[2]}"""
+        return output
+    else:
+        return None
+
+def tparser(line):
+    parser = re.search("(^[takeTAKE]*\s[profitPROFIT]*)\s\d\s[at@\s]*([\d]*[,.][0-9]*)", line)
+    if parser != None:
+        p = parser.groups()
+        output = f"""✅TP {p[-1]}"""
+        return output
+    else:
+        return None
+
+def sparser(line):
+    parser = re.search("(^[stopSTOP]*\s[lossLOSS]*)\s[at@\s]*([\d]*[.,][\d]*)", line)
+    if parser != None:
+        p = parser.groups()
+        output = f"""🛑SL {p[-1]}"""
+        return output
+    else:
+        return None
+
+def pasig(rawsignal):     
+    """entry function to parse forex signals"""
+    signal = """"""
+    rsignal = rawsignal.split("\n")
+    for line in rsignal:
+        bp = bparse(line)
+        tp = tparser(line)
+        sp = sparser(line)
+        for i in [bp, tp, sp]:
+            if i != None:
+                signal = signal + """
+"""+i
+    return signal
+    
+
+rawsignal = """
+BUY EURUSD (@ 1.0877) 
+Take profit 1 at 1.0897
+Take profit 2 at 1.0927
+Take profit 3 at 1.0977
+Stop loss at 1.0795
+"""
+
+outputsignal = """
+#EURUSD 
+BUY:chart_with_upwards_trend:1.0877
+:white_check_mark:TP 1.0897
+:white_check_mark:TP 1.0927
+:white_check_mark:TP 1.0977
+✅
+🛑SL 1.0795
+"""
+
+# print(pasig(rawsignal))
