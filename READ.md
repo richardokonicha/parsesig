d5ca17a7525e1c4237f7d62fd0733ad402d3180b
reechee <richardokonicha@gmail.com>
2020-05-11 03:09:58 +0100

    add environment variable and READme

---
+++ READ.md
@@ -0,0 +1,63 @@
+# SIGPARSER
+
+## A Telegram program that forwards Forex Signals from one Telegram group or channel to another
+
+Listens for message events on CHATINPUT channel that matches the regex pattern and parses it with pasig()
+
+REGEX
+>>^(BUY|SELL)\s([A-Z]*)\s[\(@at\s]*([0-9]*[.,][0-9]*)[\).]
+
+```comment
+                                |     #EURUSD
+BUY EURUSD (@ 1.0877)           |     BUY📉1.0877
+Take profit 1 at 1.0897         |     🛑TP 1.0897
+Take profit 2 at 1.0927         |     🛑TP 1.0927
+Take profit 3 at 1.0977         |     🛑TP 1.0977
+Stop loss at 1.07978            |     🛑SL 1.07978
+```
+
+Sigparser takes the following environment variables or configure .env file in the project directory
+Main pair
+
+  ```python
+  CHATINPUT=-12345678901234
+  CHATOUTPUT=-1234567890123
+  ```
+
+Test pair
+
+  ```python
+  TESTINPUT=-12345678901234
+  TESTOUTPUT=-1234567890123
+  ```
+
+All matching messages from the channel set in the CHATINPUT environment variable 
+is forwarded to the channel set in the CHATOUTPUT environment variable same goes for the test variables
+
+
+## Heroku setup
+
+---
+
+* Create new Heroku app on your [Heroku account](https://heroku.com).
+* Link app to using the gihub option and select the repository
+* From terminal set git heroku remote url
+  
+  ```bash
+  git remote set-url --add heroku https://git.heroku.com/{your_app_name}.git
+  heroku login
+  ```
+
+* After build and deploy succeed, check for the number of running dynos
+
+  ```bash
+  heroku ps
+  ```
+
+  *this application is define from the Procfile as a worker process, on first build number of dynos=0*
+* Set environment variables on heroku, goto settings and reveal config vars and set key:value
+  *Start up one dyno with a single process*
+
+  ```bash
+  heroku ps:scale worker=1
+  ```
--- telethon_access.py
+++ telethon_access.py
@@ -3,44 +3,74 @@ import db
 from telethon import errors
 import logging
 from parser import pasig
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
-
+# set logging level
 logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s',
                     level=logging.WARNING)
+# gets User object from db with message pair
 # message_pair = db.MessagePair.get_name('testcase')
 message_pair = db.MessagePair.get_name('BST')
 
+# defines variables from message pair object
 api_hash = message_pair.api_hash
 api_id = message_pair.api_id
 bot_token = message_pair.bot_token
-try:
-    channel_input = int(message_pair.channel_input)
-except ValueError:
-    channel_input = message_pair.channel_input
-try:
-    channel_output = int(message_pair.channel_output)
-except ValueError:
-    channel_output = message_pair.channel_output
 
-channel_input = -1001313782946
+# sets channels taking both string input and number input
+try:
+    test_input = int(os.getenv("TESTINPUT"))
+    channel_input = int(os.getenv('CHATINPUT'))
+except ValueError:
+    # channel_input = message_pair.channel_input
+    # channel_input = int(message_pair.channel_input)
+    print('couldnt get env')
+try:
+    test_output = int(os.getenv("TESTOUTPUT"))
+    channel_output = int(os.getenv('CHATOUTPUT'))
+    
+except ValueError:
+    # channel_output = int(message_pair.channel_output)
+    print('couldnt get env')
+    # channel_output = message_pair.channel_output
+
+# test variable to override channel input from db
 pair_name = message_pair.pair_name
 client = TelegramClient("BST_t", api_id, api_hash)
 
-
+# client event handler on incoming new messages matching the regex filter from chat or channel
 @client.on(
     events.NewMessage(
         chats= channel_input, 
         pattern=r"^(BUY|SELL)\s([A-Z]*)\s[\(@at\s]*([0-9]*[.,][0-9]*)[\).]", 
         incoming=True
         ))
+# function takes message text that matches the regex filter and transforms using pasig transformer and sends to new channel
 async def forwarder(event):
     text = event.message.text
     signal = pasig(text)
     print(signal)
     output_channel = await client.send_message(channel_output, signal)
 
-@client.on(events.NewMessage)
-async def fresh_message(event):
+# listens on the test channel
+@client.on(
+    events.NewMessage(
+        chats= test_input, 
+        pattern=r"^(BUY|SELL)\s([A-Z]*)\s[\(@at\s]*([0-9]*[.,][0-9]*)[\).]", 
+        incoming=True
+        ))
+# function takes message text that matches the regex filter and transforms using pasig transformer and sends to new channel
+async def forwarder(event):
     text = event.message.text
+    signal = pasig(text)
+    print(signal)
+    output_channel = await client.send_message(test_output, signal)
+
+
+# @client.on(events.NewMessage)
+# async def fresh_message(event):
+#     text = event.message.text
 client.start()
 client.run_until_disconnected()
