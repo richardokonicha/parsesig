c4502e77d42040c5e99a067a01dafb4b7f2347eb
reechee <richardokonicha@gmail.com>
2021-01-19 05:50:03 +0100

    add .env example

--- telethon_access.py
+++ telethon_access.py
@@ -2,23 +2,12 @@ from telethon import TelegramClient, events
 from telethon.sessions import StringSession
 import logging
 from parser import pasig
-import os
-from dotenv import load_dotenv
-load_dotenv()
+from config import api_hash, api_id, channel_input, channel_output, session, save_session
 # from util import bot_forward
 logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s',
                     level=logging.WARNING)
 
-channel_input = os.getenv('CHATINPUT')
-channel_input = [int(i) for i in channel_input.split(' ')]  # dont
-channel_output = int(os.getenv('CHATOUTPUT')) # dont
 
-session = os.getenv("SESSION")
-session = ''
-api_hash = os.getenv("API_HASH")
-api_id = os.getenv("API_ID")
-
-# client = TelegramClient("BST_t", api_id, api_hash)
 client = TelegramClient(StringSession(session), api_id, api_hash)
 
 # client event handler on incoming new messages matching the regex filter from chat or channel
@@ -43,4 +32,5 @@ async def wakeup(event):
     print(text)
 
 client.start()
+save_session(client, session)
 client.run_until_disconnected()
