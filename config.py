6eb83866df8e9e4361f608dedccfb5401e46f483
reechee <richardokonicha@gmail.com>
2021-02-01 08:05:33 +0100

    complete forwarder and reference tracking

--- config.py
+++ config.py
@@ -6,6 +6,7 @@ load_dotenv()
 channel_input = os.getenv('CHATINPUT')
 channel_input = [int(i) for i in channel_input.split(' ')]  # dont
 channel_output = int(os.getenv('CHATOUTPUT')) # dont
+REDISTOGO_URL = os.getenv('REDISTOGO_URL')
 
 session = os.getenv("SESSION")
 api_hash = os.getenv("API_HASH")
@@ -16,16 +17,6 @@ def save_session(client, session):
         pass
     else:
         session_string = client.session.save()
-        env_string = f"SESSION = {session_string}"
+        env_string = f"\nSESSION = {session_string}"
         with open(".env", "a") as env_file:
             env_file.write(env_string)
-
-def pushToStore(message_pair: dict) -> None:
-    with open('id_pairs.json', 'w') as outfile:
-        json.dump(message_pair, outfile)
-
-def getRefId(message_id: int) -> int:
-    with open('id_pairs.json') as json_file:
-        data = json.load(json_file)
-        id = data[str(message_id)]
-    return id
--- telethon_access.py
+++ telethon_access.py
@@ -2,15 +2,16 @@ from telethon import TelegramClient, events
 from telethon.sessions import StringSession
 from telethon.tl.functions.channels import GetMessagesRequest
 import logging
-from parser import pasig
+import redis
+from text_parser import pasig
 from datetime import datetime
-from config import api_hash, api_id, channel_input, channel_output, session, save_session, pushToStore, getRefId
+from config import api_hash, api_id, channel_input, channel_output, session, save_session, REDISTOGO_URL
 # from util import bot_forward
 logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s',
                     level=logging.WARNING)
 
-
-client = TelegramClient('StringSession(session)', api_id, api_hash)
+r = redis.from_url(url=REDISTOGO_URL)
+client = TelegramClient(StringSession(session), api_id, api_hash)
 
 # client event handler on incoming new messages matching the regex filter from chat or channel
 # function takes message text that matches the regex filter and 
@@ -18,32 +19,28 @@ client = TelegramClient('StringSession(session)', api_id, api_hash)
 @client.on(
     events.NewMessage(
         chats= channel_input, 
-        pattern=r"^(BUY|SELL)\s([A-Z]*)\s[\(@at\s]*([0-9]*[.,][0-9]*)[\).]", 
+        # pattern=r"^(BUY|SELL)\s([A-Z]*)\s[\(@at\s]*([0-9]*[.,][0-9]*)[\).]", 
         incoming=True,
-        # outgoing=True
         ))
 async def forwarder(event):
     text = event.message.text
     signal = pasig(text)
+    if bool(signal):
+        pass
+    else:
+        signal = text
 
     message_id = event.message.id
-    
-    # referenced = await client(GetMessagesRequest(
-    #     channel=channel_output,
-    #     id=[185]
-    # ))
+    reply_msg = event.message.reply_to_msg_id
 
-    ref_id = getRefId(message_id)
+    try:
+        ref = int(r.get(reply_msg).decode('utf-8'))
+    except:
+        print('Out of scope or bounds for redis')
+        ref = None
 
-    output_channel = await client.send_message(channel_output, signal, reply_to = ref_id)
-    
-    message_pair = {
-        event.message.id: output_channel.id,
-        "time": datetime
-    }
-    
-    pushToStore(message_pair)
-
+    output_channel = await client.send_message(channel_output, signal, reply_to = ref)
+    r.set(event.message.id, output_channel.id)
     print(signal)
 
 # keeps heroku from falling asleep
