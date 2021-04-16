353366a305f540a288a66c4706301daece0a49f2
reechee <richardokonicha@gmail.com>
2021-04-16 02:44:47 +0100

    added channel counter

--- main.py
+++ main.py
@@ -27,6 +27,7 @@ async def forwarder(event):
     valid = emanuelefilter(text)
     text = transform_text(text)
 
+    count = 0
     for cht in channel_output:
         try:
             ref = int(r.get(f"{cht}-{reply_msg}").decode('utf-8'))
@@ -40,12 +41,18 @@ async def forwarder(event):
             msg_file = None
             ext = None
 
+        count += 1
+        print(cht, count)
+
         if valid:
-            output_channel = await client.send_message(cht, text, file=msg_file, reply_to=ref)
-            r.set(f"{cht}-{event.message.id}", output_channel.id)
-            print(f"\u001b[32mSENT......{text}....SENT\u001b[37m....")
+            try:
+                output_channel = await client.send_message(cht, text, file=msg_file, reply_to=ref)
+                r.set(f"{cht}-{event.message.id}", output_channel.id)
+                print(f"\u001b[32mSENT......{text}....SENT\u001b[37m....")
+            except:
+                print(f"\u001b[31mNot Sent an error occurred {text[:70]} ...Not Sent\u001b[37m...") 
         else:
-            print(f"\u001b[31mNot Sent  {text[:70]} ...Not Sent\u001b[37m...") 
+            print(f"\u001b[31mNot Sent invalid {text[:70]} ...Not Sent\u001b[37m...") 
 
 @client.on(events.NewMessage)
 async def wakeup(event):
