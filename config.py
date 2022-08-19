7b08764642a12c6956c42596a7a07d9e5871bb92
r.e.e.c.h.e.e <richardokonicha@gmail.com>
2022-08-19 04:47:55 +0100

    Update config.py

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ load_dotenv()
 channel_input = os.getenv('CHATINPUT')
 channel_input = [int(i) for i in channel_input.split(' ')]  # dont
 channel_output = int(os.getenv('CHATOUTPUT')) # dont
-REDISTOGO_URL = os.getenv('REDISTOGO_URL')
+REDIS_URL = os.getenv('REDISCLOUD_URL')
 
 session = os.getenv("SESSION")
 api_hash = os.getenv("API_HASH")
