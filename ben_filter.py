e44b05a2a3f810dc49dd976eed4dae315daa1ea3
Lord Fugoku <richardokonicha@gmail.com>
2023-01-09 12:41:00 +0000

    finish hebrew text template

--- ben_filter.py
+++ ben_filter.py
@@ -4,10 +4,20 @@
 import re
 
 
+text = """
+{coin}
+{direction}
+כניסה בין {entry}
+מטרות:
+{target_list}
+סטופ: {stop_loss}
+"""
+
+
 def parse_message(input_message: str) -> str:
     # Extract the relevant information from the input message
-    signal_id = re.search(r'#(\d+)\s*', input_message,
-                          re.IGNORECASE).group(1) or 'None'
+    # signal_id = re.search(r'#(\d+)\s*', input_message,
+    #                       re.IGNORECASE).group(1) or 'None'
     coin = re.search(r'COIN:\s*\$(\w+)/', input_message,
                      re.IGNORECASE).group(1) or 'None'
     direction = re.search(r'Direction:\s*(\w+)',
@@ -32,12 +42,17 @@ def parse_message(input_message: str) -> str:
     # Convert targets to list of targets
     short_term_targets = short_term_targets.split(' - ')
     mid_term_targets = mid_term_targets.split(' - ')
+    mid_term_targets[-1] = mid_term_targets[-1].replace('\n', ' ')
     targets = short_term_targets + mid_term_targets
 
     # Create the template message
-    template_message = f'SIGNAL ID: #{signal_id}\nCOIN: {coin}\nכיוון: {direction}\nכניסה: {entry}\nמטרות:\n'
+    # template_message = f'SIGNAL ID: #{signal_id}\nCOIN: {coin}\nכיוון: {direction}\nכניסה: {entry}\nמטרות:\n'
+    target_list = ""
     for i, target in enumerate(targets, start=1):
-        template_message += f'{i} - {target}\n'
-    template_message += f'סטופ: {stop_loss}'
+        target_list += f'{i} - {target}\n'
+    # template_message += f'סטופ: {stop_loss}'
+
+    template_message = text.format(coin=coin, direction=direction,
+                                   entry=entry, stop_loss=stop_loss, target_list=target_list)
 
     return template_message
