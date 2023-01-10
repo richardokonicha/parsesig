e7fff0a95a7cc0c8cb1e463c38fe343c9c97690e
r.e.e.c.h.e.e <richardokonicha@gmail.com>
2023-01-10 18:00:25 +0100

    updated regex for multiple context
    updated benfilters for multiple structure

--- ben_filter.py
+++ ben_filter.py
@@ -1,10 +1,6 @@
 
-# filter for bentheman
 
-import re
-
-
-text = """
+finale = """
 {coin}
 {direction}
 כניסה בין {entry}
@@ -13,46 +9,61 @@ text = """
 סטופ: {stop_loss}
 """
 
+import re
 
-def parse_message(input_message: str) -> str:
-    # Extract the relevant information from the input message
-    # signal_id = re.search(r'#(\d+)\s*', input_message,
-    #                       re.IGNORECASE).group(1) or 'None'
-    coin = re.search(r'COIN:\s*\$(\w+)/', input_message,
-                     re.IGNORECASE).group(1) or 'None'
-    direction = re.search(r'Direction:\s*(\w+)',
-                          input_message, re.IGNORECASE).group(1) or 'None'
-    entry = re.search(r'ENTRY:\s*([\d.]+\s*-\s*[\d.]+)',
-                      input_message, re.IGNORECASE).group(1) or 'None'
-    ote = re.search(r'OTE:\s*([\d.]+)', input_message,
-                    re.IGNORECASE).group(1) or None
-    short_term_targets = re.search(
-        r'Short\s*Term:\s*([\d.\s*-]+)', input_message, re.IGNORECASE).group(1) or 'None'
-    mid_term_targets = re.search(
-        r'Mid\s*Term:\s*([\d.\s*-]+)', input_message, re.IGNORECASE).group(1) or 'None'
-    stop_loss = re.search(
-        r'STOP\s*LOSS:\s*([\d.]+)', input_message, re.IGNORECASE).group(1) or 'None'
+def getTargetList(text):
+  targets = []
+  target_list = ''
+  try:
+    target_re = re.search(r'(?<=Target[s\s][\d\n])([\d.\n]+)', text, re.IGNORECASE).group(1) or 'None'
+    targets = target_re.split("\n")
+    targets = [x for x in targets if x]
+    if not targets: raise Exception('Error')
+  except Exception as e:
+    targets = re.findall(r'(?<=Target[s\s][\d])[\D]+([\d.]+)', text, re.IGNORECASE)
+    if not targets:
+      short_term_targets = re.search(
+          r'Short\s*Term:\s*([\d.\s*-]+)', text, re.IGNORECASE).group(1) or 'None'
+      mid_term_targets = re.search(
+          r'Mid\s*Term:\s*([\d.\s*-]+)', text, re.IGNORECASE).group(1) or 'None'
+      short_term_targets = short_term_targets.split(' - ')
+      mid_term_targets = mid_term_targets.split(' - ')
+      mid_term_targets[-1] = mid_term_targets[-1].replace('\n', ' ')
+      targets = short_term_targets + mid_term_targets
+  except Exception as e:
+    print("Signal unrecognised", e, text)
 
+  for i, target in enumerate(targets, start=1):
+    target_list += f'{i} - {target}\n'
+
+  return target_list
+
+def benfilter(text):
+
+  try:
+    coin = re.search(r'(?<=\$|#)([A-Z]+)', text,
+                      re.IGNORECASE).group(1) or 'None'
+    direction = re.search(r'(long|short)',
+                            text, re.IGNORECASE).group(1) or 'None'
     # Convert direction to Hebrew
     if direction.lower() == 'long':
         direction = 'לונג'
     else:
         direction = 'שורד'
+    
+    entry = re.search(r'(?<=ENTRY\b)[\s\n:a-z]+([\d.]+\b([\sto]+|[\s-]+)[\d.]+\b)', text, re.IGNORECASE).group(1) or 'None'
+    stop_loss = re.search(r'(stop[\s-]loss|sl)[:\s]+([\d.]+)', text, re.IGNORECASE).group(2) or 'None'
+    target_list = getTargetList(text)
 
-    # Convert targets to list of targets
-    short_term_targets = short_term_targets.split(' - ')
-    mid_term_targets = mid_term_targets.split(' - ')
-    mid_term_targets[-1] = mid_term_targets[-1].replace('\n', ' ')
-    targets = short_term_targets + mid_term_targets
-
-    # Create the template message
-    # template_message = f'SIGNAL ID: #{signal_id}\nCOIN: {coin}\nכיוון: {direction}\nכניסה: {entry}\nמטרות:\n'
-    target_list = ""
-    for i, target in enumerate(targets, start=1):
-        target_list += f'{i} - {target}\n'
-    # template_message += f'סטופ: {stop_loss}'
-
-    template_message = text.format(coin=coin, direction=direction,
-                                   entry=entry, stop_loss=stop_loss, target_list=target_list)
-
+    template_message = finale.format(
+      coin=coin,
+      direction=direction,
+      entry=entry,
+      stop_loss=stop_loss,
+      target_list=target_list
+      )
     return template_message
+    
+  except Exception as e:
+    print("Signal unrecognised", e, text)
+  
