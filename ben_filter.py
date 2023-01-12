8b3ecb2f9d54d1c0fb3a5e8a636b21a9955166d9
Lord Fugoku <richardokonicha@gmail.com>
2023-01-12 09:01:21 +0000

    identify message source and filter accordingly

--- ben_filter.py
+++ ben_filter.py
@@ -1,5 +1,3 @@
-
-
 import re
 finale = """
 {coin}
@@ -10,57 +8,75 @@ finale = """
 סטופ: {stop_loss}
 """
 
+def get_common_value(text):
+    coin = re.search(r'(?<=\$|#)([A-Z]+)', text,
+                     re.IGNORECASE).group(1) or 'None'
+    direction = re.search(r'(long|short)',
+                          text, re.IGNORECASE).group(1) or 'None'
+    # Convert direction to Hebrew
+    if direction.lower() == 'long':
+        direction = 'לונג'
+    else:
+        direction = 'שורד'
 
-def getTargetList(text):
-    targets = []
+    entry = re.search(
+        r'(?<=ENTRY\b)[\s\n:a-z*]+([\d.]+\b([\sto]+|[\s-]+)[\d.]+\b)', text, re.IGNORECASE).group(1) or 'None'
+    stop_loss = re.search(
+        r'(stop[\s-]loss|sl)[\\n:\s*]+([\d.]+)', text, re.IGNORECASE).group(2) or 'None'
+
+    value = {
+        coin: coin,
+        direction: direction,
+        stop_loss: stop_loss,
+        entry: entry
+    }
+    return value
+
+
+def parse_message(text):
     target_list = ''
-    try:
+    targets = []
+
+    if re.search(r'Bitcoin Bullets', text, re.IGNORECASE):
+        coin, direction, stop_loss, entry = get_common_value(text)
+        entry = entry.replace("to", "-")
         target_re = re.search(
             r'(Target[s\s*\\n]+)([\d.\n]+)', text, re.IGNORECASE).group(2) or 'None'
         targets = target_re.split("\n")
         targets = [x for x in targets if x]
-        if not targets:
-            print('Error')
-    except Exception as e:
+        print("Bitcoin Bullets")
+
+    elif re.search(r'Russian Insiders',  text, re.IGNORECASE):
+        coin, direction, stop_loss, entry = get_common_value(text)
         targets = re.findall(
             r'(?<=Target[s\s][\d])[\D]+([\d.]+)', text, re.IGNORECASE)
-        if not targets:
-            short_term_targets = re.search(
-                r'Short\s*Term:\s*([\d.\s*-]+)', text, re.IGNORECASE).group(1) or 'None'
-            mid_term_targets = re.search(
-                r'Mid\s*Term:\s*([\d.\s*-]+)', text, re.IGNORECASE).group(1) or 'None'
-            short_term_targets = short_term_targets.split(' - ')
-            mid_term_targets = mid_term_targets.split(' - ')
-            mid_term_targets[-1] = mid_term_targets[-1].replace('\n', ' ')
-            targets = short_term_targets + mid_term_targets
-    except Exception as e:
-        print("Signal unrecognised", e, text)
+        print("Russian Insider")
+
+    elif re.search(r'Long Entry Zone', text, re.IGNORECASE):
+        coin, direction, stop_loss, entry = get_common_value(text)
+        targets = re.findall(
+            r'(?<=Target[s\s][\d])[\D]+([\d.]+)', text, re.IGNORECASE)
+        print("Long Entry Zone")
+
+    elif re.search(r'SIGNAL ID', text, re.IGNORECASE):
+        coin, direction, stop_loss, entry = get_common_value(text)
+        short_term_targets = re.search(
+            r'Short\s*Term:\s*([\d.\s*-]+)', text, re.IGNORECASE).group(1) or 'None'
+        mid_term_targets = re.search(
+            r'Mid\s*Term:\s*([\d.\s*-]+)', text, re.IGNORECASE).group(1) or 'None'
+        short_term_targets = short_term_targets.split(' - ')
+        mid_term_targets = mid_term_targets.split(' - ')
+        mid_term_targets[-1] = mid_term_targets[-1].replace('\n', ' ')
+        targets = short_term_targets + mid_term_targets
+        print('SIGNAL ID Binance killers')
+    else:
+        print('Unrecognized signal', text)
+        return False
 
     for i, target in enumerate(targets, start=1):
         target_list += f'{i} - {target}\n'
 
-    return target_list
-
-
-def parse_message(text):
-
     try:
-        coin = re.search(r'(?<=\$|#)([A-Z]+)', text,
-                         re.IGNORECASE).group(1) or 'None'
-        direction = re.search(r'(long|short)',
-                              text, re.IGNORECASE).group(1) or 'None'
-        # Convert direction to Hebrew
-        if direction.lower() == 'long':
-            direction = 'לונג'
-        else:
-            direction = 'שורד'
-
-        entry = re.search(
-            r'(?<=ENTRY\b)[\s\n:a-z*]+([\d.]+\b([\sto]+|[\s-]+)[\d.]+\b)', text, re.IGNORECASE).group(1) or 'None'
-        stop_loss = re.search(
-            r'(stop[\s-]loss|sl)[\\n:\s*]+([\d.]+)', text, re.IGNORECASE).group(2) or 'None'
-        target_list = getTargetList(text)
-
         template_message = finale.format(
             coin=coin,
             direction=direction,
@@ -69,6 +85,5 @@ def parse_message(text):
             target_list=target_list
         )
         return template_message
-
     except Exception as e:
-        return None
+        return False
--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 import time
 from ben_filter import parse_message
 from config import (REDIS_URL, api_hash, api_id, channel_input,
-                    channel_output, session)
+                    channel_output, session, sentry_env)
 from telethon.tl.functions.channels import GetMessagesRequest
 from telethon.sessions import StringSession
 from telethon import TelegramClient, events
@@ -12,11 +12,7 @@ from datetime import datetime
 import sentry_sdk
 import re
 sentry_sdk.init(
-    dsn="https://99d6c429276948bd9e71cf6c80d91885@o4504248900583424.ingest.sentry.io/4504248905236480",
-
-    # Set traces_sample_rate to 1.0 to capture 100%
-    # of transactions for performance monitoring.
-    # We recommend adjusting this value in production.
+    dsn=sentry_env,
     traces_sample_rate=1.0
 )
 
@@ -34,46 +30,48 @@ client = TelegramClient(StringSession(session), api_id, api_hash)
         outgoing=True
     ))
 async def forwarder(event):
-    text = event.message.text
-    message_id = event.message.id
-    reply_msg = event.message.reply_to_msg_id
+    try:
+        text = event.message.text
+        message_id = event.message.id
+        reply_msg = event.message.reply_to_msg_id
 
-    text = parse_message(text)
-    valid = bool(text)
-    count = 0
-    for cht in channel_output:
-        try:
-            ref = int(r.get(f"{cht}-{reply_msg}").decode('utf-8'))
-        except:
-            print('Out of scope or bounds for redis')
-            ref = None
-        try:
-            msg_file = event.message.file.media
-            ext = event.message.file.ext
-        except:
-            msg_file = None
-            ext = None
-
-        count += 1
-
-        print(cht, count)
-
-        if valid:
+        cleaned_text = parse_message(text)
+        valid = bool(cleaned_text)
+        count = 0
+        for cht in channel_output:
             try:
-                output_channel = await client.send_message(cht, text, file=msg_file, reply_to=ref)
-                r.set(f"{cht}-{event.message.id}", output_channel.id)
-                print(f"\u001b[32mSENT......{text}....SENT\u001b[37m....")
-            except ConnectionRefusedError:
-                print(f"\u001b[31mRedis broke\u001b[37m...")
-            except Exception as e:
+                ref = int(r.get(f"{cht}-{reply_msg}").decode('utf-8'))
+            except:
+                print('Out of scope or bounds for redis')
+                ref = None
+            try:
+                msg_file = event.message.file.media
+                ext = event.message.file.ext
+            except:
+                msg_file = None
+                ext = None
+
+            count += 1
+            print(cht, count)
+            if valid:
+                try:
+                    output_channel = await client.send_message(cht, cleaned_text, file=msg_file, reply_to=ref)
+                    print(
+                        f"\u001b[32mSENT......{cleaned_text}....SENT\u001b[37m....")
+                    r.set(f"{cht}-{event.message.id}", output_channel.id)
+                except redis.exceptions.ConnectionError as e:
+                    print(f"\u001b[31mRedis broke\u001b[37m...", e)
+                except Exception as e:
+                    print(
+                        f"\u001b[31mNot Sent an error occurred {text[:min(len(text), 50)]} ...Not Sent {e}\u001b[37m...")
+            else:
                 print(
-                    f"\u001b[31mNot Sent an error occurred {text[:70]} ...Not Sent {e}\u001b[37m...")
-        else:
-            print(
-                f"\u001b[31mNot Sent invalid {text[:70]} ...Not Sent\u001b[37m...")
+                    f"\u001b[31mNot Sent invalid {text[:min(len(text), 50)]} ...Not Sent\u001b[37m...")
+    except Exception as e:
+        print("Ignored message", e)
 
 
-@client.on(events.NewMessage)
+@ client.on(events.NewMessage)
 async def wakeup(event):
     print('Active...')
 
