2ced36f727daf8cf14f0eacf5c4492acd79d1656
Lord Fugoku <richardokonicha@gmail.com>
2023-01-17 17:14:17 +0000

    hard coded env for ben

--- config.py
+++ config.py
@@ -3,9 +3,12 @@ import json
 from dotenv import load_dotenv
 load_dotenv()
 
+
 chinput = os.getenv('CHATINPUT')
+chinput = '-1001799753250 -1001574745581 -1001322515232 -1001725353361'
 channel_input = [int(i) for i in chinput.split(' ')]
 choutput = os.getenv('CHATOUTPUT')
+choutput = '-1001802541407'
 channel_output = [int(i) for i in choutput.split(' ')]
 
 
