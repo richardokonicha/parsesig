7535c56fa93a3feeb5716268fe055ed2c353d551
Lord Fugoku <richardokonicha@gmail.com>
2023-04-18 04:44:21 +0000

    fix output duplicate

--- config.py
+++ config.py
@@ -11,7 +11,7 @@ channel_input.append(int(chinput2))
 choutput = os.getenv('CHATOUTPUT')
 choutput2 = os.getenv('CHATOUTPUT2')
 channel_output = [int(i) for i in choutput.split(' ')]
-channel_output.append(int(choutput))
+if choutput2: channel_output.append(int(choutput2))
 
 REDIS_URL = os.getenv('REDIS_URL')
 session = os.getenv("SESSION")
